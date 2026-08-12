3681330e2111a040ff46ce268b8df851eabf60b6
Florian Graßinger <grassinger@ubyte.gmbh>
2026-08-12 17:53:43 +0200

    SmartDeviceMonitor: Fix incorrect moduleID for DeviceRegistry selection

--- SmartDeviceMonitor/module.php
+++ SmartDeviceMonitor/module.php
@@ -291,7 +291,7 @@ HTML;
                     "type"     => "SelectModule",
                     "name"     => "RegistryID",
                     "caption"  => "Device Registry (Geraeteverwaltung)",
-                    "moduleID" => "{C1F5C0DF-2FEE-450C-920E-5C138E66270E}"
+                    "moduleID" => "{F3B4A7D9-C59E-401A-B826-17D3B5C2849E}"
                 ],
                 [
                     "type"    => "Label",
