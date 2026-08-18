3310f55809d64b76db00248e6c76e66c07c6cd74
Florian Graßinger <grassinger@ubyte.gmbh>
2026-08-18 22:42:26 +0200

    chore: remove scratch files

--- scratch.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once '/var/lib/symcon/scripts/__ipsmodule.inc.php';
-\ = IPS_GetInstanceListByModuleID('{F3B4A7D9-C59E-401A-B826-17D3B5C2849E}')[0];
-echo IPS_GetProperty(\, 'DevicesGenericSensor');
